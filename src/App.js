f299522fc3ab8ead31296dfde87989c511c7e90d
HSM21 <hemasundharmoola33@gmail.com>
2024-07-16 22:26:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Practice from './components/SocialMedia';
+
+function App() {
+  return (
+    <div className="App">
+      <Practice/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialMedia/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index.css";
+
+
+const Practice = () => {
+  return (
+    <div className="container">
+        <div>
+            <h1 className="socialbuttons">Social Buttons</h1>
+        </div>
+        <div className="responsebuttons">
+          <button className="like">Like</button>
+          <button className="comment">Comment</button>
+          <button className="share">Share</button>
+        </div>
+     
+    </div>
+  );
+};
+
+export default Practice;
